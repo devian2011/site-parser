e3789bcccd3cce32468b3bd4d27f9c5657d133e9
devian <romanov@auwi.su>
2017-03-10 14:23:27 +0300

    Добавьте файлы проекта.

---
+++ SiteParser/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace SiteParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string domainUrl = "http://www.velosite.local:3114";
+            WebClient wClient = new WebClient();
+            wClient.BaseAddress = domainUrl;
+            string htmlContent = wClient.DownloadString("/");
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(htmlContent);
+            foreach( HtmlAgilityPack.HtmlNode node in doc.DocumentNode.SelectNodes("//a[@href]"))
+            {
+                Console.WriteLine(node.Attributes["href"].Value);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SiteParser/Services/Parser/SiteMap/SiteMapRowModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SiteParser.Services.Parser.SiteMap
+{
+    class SiteMapRowModel
+    {
+        public string location { get; set; }
+        public string lastModified { get; set; }
+        public string changeFrequency { get; set; }
+        public string priority { get; set; }
+    }
+}
